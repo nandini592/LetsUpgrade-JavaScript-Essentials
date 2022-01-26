75ee645df5389faf02bf1358bffa8384465f9cf6
nandini592 <68197831+nandini592@users.noreply.github.com>
2022-01-26 13:26:45 +0530

    Add files via upload

---
+++ js_asgn1/first_asgn1.js
@@ -0,0 +1,7 @@
+const radius = prompt('input radius');
+console.log('radius of circle: ' + radius);
+
+const PI = 3.14;
+const areaofcircle = PI * radius * radius;
+
+console.log('area of circle with given radius is: ' + areaofcircle);
